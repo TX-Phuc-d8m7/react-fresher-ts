a6fda49c88dda97e51667685fb09d648d1f12865
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-08-16 17:44:10 +0700

    [UPDATE] React Router Setup

---
+++ src/components/layout/app.header.tsx
@@ -0,0 +1,9 @@
+const AppHeader = () => {
+  return (
+    <div className="header">
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default AppHeader;
--- src/layout.tsx
+++ src/layout.tsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+import AppHeader from "./components/layout/app.header";
+
 function Layout() {
   return (
     <>
-      <div>Hello Work Updated</div>
+      <AppHeader />
+      <Outlet />
     </>
   );
 }
