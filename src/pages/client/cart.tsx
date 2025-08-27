84ef5b67049fccc828faecc7b4afea1b9445f9ef
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-08-27 22:58:20 +0700

    Daily Commit

---
+++ src/pages/client/cart.tsx
@@ -0,0 +1,10 @@
+const BookPage = () => {
+    return (
+        <div>
+            <h1>Cart Page</h1>
+            <p>This is the cart page</p>
+        </div>
+    );
+};
+
+export default BookPage;
