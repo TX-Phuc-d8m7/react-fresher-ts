35cc05f1077f814e49fb58a71d0e1afa9740055a
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-08-28 23:47:57 +0700

    Daily Commit

---
+++ src/pages/client/bin.tsx
@@ -0,0 +1,10 @@
+const BookPage = () => {
+    return (
+        <div>
+            <h1>Bin Page</h1>
+            <p>This is the bin page</p>
+        </div>
+    );
+};
+
+export default BookPage;
