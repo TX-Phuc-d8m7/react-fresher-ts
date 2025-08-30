dccbaefb32cd7d0f4cd8ff31b18f0b18e6481802
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-08-30 21:07:33 +0700

    Daily Commit

---
+++ src/pages/client/policy.tsx
@@ -0,0 +1,10 @@
+const PolicyPage = () => {
+    return (
+        <div>
+            <h1>Policy Page</h1>
+            <p>This is the policy page</p>
+        </div>
+    );
+};
+
+export default PolicyPage;
