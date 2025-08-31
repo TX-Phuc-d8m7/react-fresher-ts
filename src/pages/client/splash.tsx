ff86d1b30b77f1bac5210e10fd0d5648af0f1dc9
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-08-31 22:26:21 +0700

    Daily Commit

---
+++ src/pages/client/splash.tsx
@@ -0,0 +1,10 @@
+const SplashPage = () => {
+    return (
+        <div>
+            <h1>Splash Page</h1>
+            <p>This is the Splash page</p>
+        </div>
+    );
+};
+
+export default SplashPage;
