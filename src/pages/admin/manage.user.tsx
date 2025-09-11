8a837765c6dd690d714c82fb73eba44dcb0d7f98
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-09-11 23:58:04 +0700

    Daily Commit

--- src/pages/admin/manage.user.tsx
+++ src/pages/admin/manage.user.tsx
@@ -3,7 +3,7 @@ import TableUser from "@/components/admin/user/table.user";
 const ManageUserPage = () => {
     return (
         <div>
-            <h1>Manage User Page</h1>
+            <h1>Manage User Information Page</h1>
             <p>This is the manage user page</p>
         </div>
     )
