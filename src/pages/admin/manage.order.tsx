60f2cc86f1441383fbb4b5aaabd9036a83e7fe27
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-09-14 22:28:23 +0700

    Daily Commit

--- src/pages/admin/manage.order.tsx
+++ src/pages/admin/manage.order.tsx
@@ -2,7 +2,7 @@ const ManageOrderPage = () => {
     return (
         <div>
             <h1>Manage Order Page</h1>
-            <p>This is the manage order page</p>
+            <p>This is the manage order page where we can add, edit or delete client's orders</p>
         </div>
     )
 }
