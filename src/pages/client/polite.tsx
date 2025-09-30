8f25ba02fee1c759813b4e1cb83272a708bacf49
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-09-30 23:25:58 +0700

    Daily Commit

---
+++ src/pages/client/polite.tsx
@@ -0,0 +1,10 @@
+const PolitePage = () => {
+    return (
+        <div>
+            <h1>Polite Page</h1>
+            <p>This is the polite page</p>
+        </div>
+    );
+};
+
+export default PolitePage;
