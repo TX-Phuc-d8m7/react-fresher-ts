49f381fcc84d9bbbf588e44d243f14b64b079141
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-10-01 23:36:04 +0700

    Daily Commit

---
+++ src/pages/client/back.tsx
@@ -0,0 +1,10 @@
+const BackPage = () => {
+    return (
+        <div>
+            <h1>Back Page</h1>
+            <p>This is the back page</p>
+        </div>
+    );
+};
+
+export default BackPage;
