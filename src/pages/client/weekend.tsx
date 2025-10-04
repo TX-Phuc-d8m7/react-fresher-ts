ef53b46a04c9f5a8f4691f252f72df87fb5dbeaa
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-10-04 22:58:20 +0700

    Daily Commit

---
+++ src/pages/client/weekend.tsx
@@ -0,0 +1,10 @@
+const WeekendPage = () => {
+    return (
+        <div>
+            <h1>WeekendPage</h1>
+            <p>This is the weekend page</p>
+        </div>
+    );
+};
+
+export default WeekendPage;
